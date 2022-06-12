cdc55d62a5710ed5f36ed9c8d8c7570b832e7a7d
Vitold S <vit1251@gmail.com>
2022-06-12 16:31:26 +0300

    Exec now is execute in async mode

--- src/awe-sqlite3-exec.c
+++ src/awe-sqlite3-exec.c
@@ -8,6 +8,50 @@
 
 #include "awe-sqlite3.h"
 
+struct awe_sqlite3_exec {
+  napi_async_work worker;
+  napi_deferred deferred;
+  sqlite3 *db;
+  char *query;
+  int code;
+};
+
+static void
+awe_sqlite3_exec_work(napi_env env, void* data) {
+  struct awe_sqlite3_exec *arg = (struct awe_sqlite3_exec *)data;
+  int rc;
+  char *err_msg = NULL;
+
+  /* Step 1. Perform SQL query */
+  rc = sqlite3_exec(arg->db, arg->query, 0, 0, &err_msg);
+  if (rc != SQLITE_OK) {
+    fprintf(stdout, "error: %s", err_msg);
+  }
+
+  /* Step 2. Save error code */
+  arg->code = rc;
+
+}
+
+static void
+awe_sqlite3_exec_done(napi_env env, napi_status status, void* data) {
+  struct awe_sqlite3_exec *arg = (struct awe_sqlite3_exec *)data;
+  napi_value ret;
+
+  napi_create_int32(env, arg->code, &ret);
+
+  if (arg->db != NULL) {
+    napi_resolve_deferred(env, arg->deferred, ret);
+  } else {
+    napi_reject_deferred(env, arg->deferred, ret);
+  }
+  napi_delete_async_work(env, arg->worker);
+  arg->deferred = NULL;
+
+  /* Release memory */
+  free(arg->query);
+  free(arg);
+}
 
 napi_value
 awe_sqlite3_exec(napi_env env, napi_callback_info info) {
@@ -18,8 +62,7 @@ awe_sqlite3_exec(napi_env env, napi_callback_info info) {
   napi_status status;
   struct awe_sqlite3 *userdata = NULL;
   char *query = NULL;
-  int rc;
-  char *err_msg = NULL;
+
 
   status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
   if (status != napi_ok) {
@@ -30,7 +73,7 @@ awe_sqlite3_exec(napi_env env, napi_callback_info info) {
   if (status != napi_ok) {
 //      return NULL;
   }
-  
+
   query = (char *)malloc(65536);
   status = napi_get_value_string_utf8(env,
                                        argv[1],
@@ -41,10 +84,14 @@ awe_sqlite3_exec(napi_env env, napi_callback_info info) {
   /* Step 2. Debug message */
   fprintf(stdout, "debug: Exec SQLite3 query on connection #%d with SQL = %s\n", userdata->index, query);
 
-  rc = sqlite3_exec(userdata->db, query, 0, 0, &err_msg);
-  if (rc != SQLITE_OK) {
-    // TODO - ...
-  }
+  /* Step 3. Setup promise about close connection */
+  struct awe_sqlite3_exec *ptr = (struct awe_sqlite3_exec *)malloc(sizeof(struct awe_sqlite3_exec));
+  ptr->db = userdata->db;
+  ptr->query = query;
 
-  return NULL;
+  NAPI_CALL(env, napi_create_promise(env, &ptr->deferred, &promise));
+  NAPI_CALL(env, napi_create_string_utf8(env, "awe-sqlite3-exec", NAPI_AUTO_LENGTH, &resource_name));
+  NAPI_CALL(env, napi_create_async_work(env, NULL, resource_name, awe_sqlite3_exec_work, awe_sqlite3_exec_done, (void *)ptr, &ptr->worker));
+  NAPI_CALL(env, napi_queue_async_work(env, ptr->worker));
+  return promise;
 }
--- test/sqlite3-open.test.js
+++ test/sqlite3-open.test.js
@@ -15,14 +15,47 @@ describe('Awe SQLite3', () => {
   });
 
   describe('exec', () => {
-    it('should OK processing on valid SQL query', async () => {
-      const conn1 = await open('./2.sqlite3');
+  
+    it('should OK when processing valid SQL query', async () => {
+      const conn1 = await open('./exec_1.sqlite3');
       await exec(conn1, "CREATE TABLE contacts (" +
          "contact_id INTEGER PRIMARY KEY, " +
          "first_name TEXT NOT NULL" +
          ");");
       await close(conn1);
     });
+
+    it('should OK when processing 100 sequential valid SQL query', async () => {
+      const conn1 = await open('./exec_2.sqlite3');
+      await exec(conn1, "CREATE TABLE contacts (" +
+         "contact_id INTEGER PRIMARY KEY, " +
+         "first_name TEXT NOT NULL" +
+         ");");
+      for (let i = 0; i < 100; i++) {
+        const code = await exec(conn1, `INSERT INTO contacts (contact_id, first_name) VALUES (${i}, 'user_${i}')`);
+        console.log(code);
+      }
+      await close(conn1);
+    });
+
+    it('should OK when processing 100 parallel valid SQL query', async () => {
+      const conn1 = await open('./exec_3.sqlite3');
+      await exec(conn1, "CREATE TABLE contacts (" +
+         "contact_id INTEGER PRIMARY KEY, " +
+         "first_name TEXT NOT NULL" +
+         ");");
+      const promises = [];
+      for (let i = 0; i < 100; i++) {
+        const query = `INSERT INTO contacts (contact_id, first_name) VALUES (${i}, 'user_${i}')`;
+        const promise = exec(conn1, query);
+        promises.push(promise);
+      }
+      const resolves = await Promise.all(promises);
+      console.log(resolves);
+      await close(conn1);
+    });
+
+
   });
 
   describe('close()', () => {
